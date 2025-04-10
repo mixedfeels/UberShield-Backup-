c83a089ba05cd354bdebc33cb0e4fa03cf688594
izzy <162716214+IsraelRibeiro05@users.noreply.github.com>
2025-04-10 14:20:11 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/ubershield/MainActivity.java
+++ app/src/main/java/com/example/ubershield/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.ubershield;
 
+import static android.os.Build.VERSION_CODES.R;
+
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -16,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
