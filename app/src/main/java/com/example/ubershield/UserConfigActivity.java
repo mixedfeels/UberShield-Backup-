73c4091e78c794e4b863b19b253dac9d611624bc
izzy <162716214+IsraelRibeiro05@users.noreply.github.com>
2025-04-22 20:00:33 -0300

    adicionei funcionalidade ao botao
    funcionaldiade ao botao da useractivity e exclui a maps qja que nao vamos mais usar

---
+++ app/src/main/java/com/example/ubershield/UserConfigActivity.java
@@ -0,0 +1,25 @@
+package com.example.ubershield;
+import android.content.Intent;
+import android.widget.ImageButton;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class UserConfigActivity extends AppCompatActivity {
+
+    private ImageButton voltarBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_config);
+
+        //unico codigo, usei pro voltarBtn
+        voltarBtn = findViewById(R.id.btnBack);
+        //eeh, o codigo basico pra ir pra home ao clicar pra voltar
+        voltarBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(UserConfigActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
+
+
+}
