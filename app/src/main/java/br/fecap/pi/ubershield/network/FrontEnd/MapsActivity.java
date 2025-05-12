a755cff02e535b8fdab9091ac82a8d7642670acd
mixedfeels <israelramospessoal@gmail.com>
2025-05-12 20:43:23 -0300

    Delete UberShield.APK
    tirei o apk velho, para colocar o novo.

--- app/src/main/java/br/fecap/pi/ubershield/network/FrontEnd/MapsActivity.java
+++ app/src/main/java/br/fecap/pi/ubershield/network/FrontEnd/MapsActivity.java
@@ -5,7 +5,6 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
